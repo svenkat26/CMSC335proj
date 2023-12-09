d89afd3e750c57d0bc9d2cfdf358fdcf16ee5937
Dinelka Jagoda <djagoda@terpmail.umd.edu>
2023-12-08 19:27:15 -0500

    7:27

---
+++ weather.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+require("dotenv").config({ path: path.resolve(__dirname, '/.env') })  
+
+const uri = 'mongodb+srv://djagoda:mongo20041@cluster0.d9s4xfa.mongodb.net/?retryWrites=true&w=majority';
+ 
+/* Our database and collection */
+const databaseAndCollection = { db: "CMSC335_DB", collection: "campApplicants"};
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1});
+
+async function main() {
+    try {
+        await client.connect();
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+}
+
+/* View engine */
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', '.ejs')
+
+/* App */
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) 
+app.use(express.static(path.join(__dirname, 'public')))
+
+/* GET Home page */
+app.get('/', (req, res) => {
+	res.render('homePage')
+});
+
+/* Get Create Account page */
+app.get('/createAccount', (req, res) => {
+	res.render('createAccount')
+});
+
+/* Terminal */
+const PORT = process.argv[2];
+app.listen(PORT)
+console.log(`Web server started and running at http://localhost:${PORT}`);
+console.log("Stop to shutdown the server: ");
+
+process.stdin.on("readable", () => {
+    let dataInput = process.stdin.read();
+    if (dataInput !== null) {
+      if (dataInput.toString().trim() === "stop") {
+        console.log("Shutting down the server\n");
+        process.exit(0);
+      } else {
+        console.log(`Invalid command: ${dataInput.toString().trim()}\n`);
+      }
+      console.log("Stop to shutdown the server: ");
+      process.stdin.resume();  
+    }
+});
+
+module.exports = app
